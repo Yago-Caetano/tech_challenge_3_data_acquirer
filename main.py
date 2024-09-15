6b09f244109219e060b48c59aebc4d0e10c76a04
Yago-Caetano <yago_caetano@outlook.com>
2024-09-15 18:44:53 -0300

    feat - removed csv file

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def get_latests():
     if(len(filtered_data) > 0):
         out_df = process_json_list(filtered_data)
         #print(out_df.head())
-        out_df.to_csv('meu_dataset.csv', index=False, encoding='utf-8',na_rep='null')
+        #out_df.to_csv('meu_dataset.csv', index=False, encoding='utf-8',na_rep='null')
         save_to_parquet(out_df)
 
 if __name__ == "__main__":
